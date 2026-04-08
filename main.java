d21e47580195c6359306395aac8d703368008680
MAICO DANIEL DA SILVA FLECK <14537119969@edu.udesc.br>
2026-04-08 19:12:50 -0300

    first commit

---
+++ main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+
+public class main {
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println( "Seu nome:");
+        String Nome = entrada.nextLine();
+
+         System.out.println( "Sua idade:");
+         int idade = entrada.nextInt();
+        entrada.nextLine();
+
+          System.out.println( "Seu tamanho:");
+          double Tamanho = entrada.nextDouble();
+          entrada.nextLine();
+
+          entrada.close();
+
+          System.out.println ("\n teste " + nome + "nome" + idade + "idade" + Tamanho + "tamanho");
+    }
+}
+
+
+  
+        //System.out.println("Hello World");
+        //String nome = "fulano";
+
+        //System.out.println( "O tamanho do nome é:");
+        //System.out.println(nome.length());
+        //System.out.println( "O nome em caixa alta é:");
+        //System.out.println(nome.toUpperCase());
+        // PARTE 1 SOBRE MAIN
+
+       // int[] inteiros = { 10, 20, 30};
+        //System.out.println("O primero valor:");
+        //System.out.println(inteiros[0]);
+
+       // System.out.println("O segundo valor:");
+        //System.out.println(inteiros[1]);
+
+        //System.out.println("O terceiro valor:");
+        //System.out.println(inteiros[2]);
+
+
+
