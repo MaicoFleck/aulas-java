470f532eb627aa4aa11cb66d682ad767e2f5860c
MaicoFLeck <maicofleck10@gmail.com>
2026-04-14 21:50:30 -0300

    exerciciosED

---
+++ exerciciosED.java
@@ -0,0 +1,30 @@
+//Incremento e decremento
+
+public class exerciciosED {
+    public static void main(String[] args) {
+        int x =1;
+        System.out.println(x);
+        x+=1;
+        System.out.println(x);
+        x+=1;
+        System.out.println(x);
+        x+=1;
+        System.out.println(x);
+        x+=1;
+        System.out.println(x);
+        x+=1;
+        System.out.println(x);
+        x+=1;
+        System.out.println(x);
+        x+=1;
+        System.out.println(x);
+        x+=1;
+        System.out.println(x);
+        x+=1;;
+        System.out.println(x);
+        x+=1;
+
+        
+
+    }
+}
---
+++ exerciciosED2.java
@@ -0,0 +1,52 @@
+
+
+public class exerciciosED2 {
+    public static void main(String[] args) {
+       // int x = 20;
+        //System.out.println(x);
+        //x--;
+        //System.out.println(x);
+       // x--;
+        //System.out.println(x);
+       // x--;
+        //System.out.println(x);
+       // x--;
+       // System.out.println(x);
+       // x--;
+       // System.out.println(x);
+       // x--;
+       // System.out.println(x);
+
+
+        int x = 2;
+        
+        System.out.println(x);
+        x+=2;
+        System.out.println(x);
+        x+=2;
+        System.out.println(x);
+        x+=2;
+        System.out.println(x);
+        x+=2;
+        System.out.println(x);
+        x+=2;
+        System.out.println(x);
+        x+=2;
+        System.out.println(x);
+        x+=2;
+        System.out.println(x);
+        x+=2;
+        System.out.println(x);
+        x+=2;
+        System.out.println(x);
+        x+=2;
+        
+
+
+
+
+
+    }
+}
+
+
